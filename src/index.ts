2d5b44dcb799ed82ca755a6e129cc974d186cf6d
Md-Imran75 <ih912137@gmail.com>
2025-03-16 05:03:57 +0600

    type add

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.get("/", (_req, res) => {
 // Only start the server if not running tests
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`🚀 Server running on port: ${PORT}`));
 }
 
 export default app;
